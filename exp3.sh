81e5d22f14e5d21adc8281d546d3db0c45c485a3
coder-Sabarish <studysabarish@gmail.com>
2023-05-19 11:10:02 +0530

    dfesarf

---
+++ exp3.sh
@@ -0,0 +1,16 @@
+echo "current user"
+who
+echo -e "\ncurrent login user"
+users
+echo "current shell"
+echo $SHELL
+echo "current directory"
+echo $HOME
+echo "os type"
+uname
+echo "path settings"
+echo $PATH
+echo "current path"
+pwd
+echo "current no of users logged in"
+users | wc -w
